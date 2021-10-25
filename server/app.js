acc9d7bad97729bdbcb76e616cf2b56a9e33d6af
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-25 14:26:57 -0700

    Creates custom token for Morgan to both log data to console and then log data to a .csv file.

--- server/app.js
+++ server/app.js
@@ -1,20 +1,34 @@
 const express = require('express');
+const morgan = require('morgan');
 const fs = require('fs');
+var path = require('path');
+
 const app = express();
 
-app.use((req, res, next) => {
-// write your logging code here
 
-});
+//removes commas from 'user-agent' header
+morgan.token('custom-user-agent', (req) => {
+  let customUserserAgent =(req.headers['user-agent'].replace(/,/g, ''));
+  return customUserserAgent;
+})
+
+morgan.token('custom', ':custom-user-agent,:date[iso],:method,:url,HTTP/:http-version,:status');
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.csv'), { flags: 'a' })
+app.use(morgan('custom'));
+app.use(morgan('custom', { stream: accessLogStream }));
 
 app.get('/', (req, res) => {
 // write your code to respond "ok" here
+  res.status(200).send("ok");
 
 });
 
 app.get('/logs', (req, res) => {
 // write your code to return a json object containing the log data here
-
+  res.json()
 });
+
+
+
 
 module.exports = app;
