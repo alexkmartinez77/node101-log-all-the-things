bae8009e5167134133e74ce826454149a5dbfe33
Alex Martinez <alexkmartinez77@gmail.com>
2022-04-12 13:11:44 -0700

    Reformats code to use fs.appendFile.

--- server/app.js
+++ server/app.js
@@ -1,40 +1,26 @@
 const express = require('express');
-const morgan = require('morgan');
 const fs = require('fs');
-const path = require('path');
 const csvToJson = require('convert-csv-to-json');
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.csv'), { flags: 'a' });
 
 const app = express();
 
 app.set('json spaces', 2);
 
-//morgan setup
-//morgan.token('custom-user-agent', (req) => {
-//  let customUserserAgent =(req.headers['user-agent'].replace(/;/g, ''));
-//  return customUserserAgent;
-//})
-//morgan.token('custom', ':custom-user-agent;:date[iso];:method;:url;HTTP/:http-version;:status');
-////app.use(morgan('custom'));
-//app.use(morgan('custom', { stream: accessLogStream }));
-
 app.get('/', (req, res) => {
-
-  let log = (`${req.header('user-agent').replace(/;/g, '')};${new Date().toISOString};${req.method};${req.originalUrl};HTTP:/${req.httpVersion};${res.statusCode}`)
-
-  fs.appendFile('./server/log.csv', `\n${log}`, 
-    (err) => err ? console.log(err) : console.log('Successful log!')
-  )
+  //create log string
+  let log = (`${req.header('user-agent').replace(/;/g, '')};${new Date().toISOString()};${req.method};${req.originalUrl};HTTP/${req.httpVersion};${res.statusCode}`)
+  //to pass the tests
+  console.log(log);
+  //append log to log.csv file
+  fs.appendFile('./server/log.csv', `\n${log}`, (err) => err ? console.log(err) : console.log('Successful log!'))
+  //response to root route
   res.send("ok");
-
 });
 
 app.get('/logs', (req, res) => {
-
+  //Convert csv file to json
   let jsonLog = csvToJson.getJsonFromCsv('./server/log.csv');
-  console.log(jsonLog.toString());
   res.json(jsonLog);
 
 });
-
 module.exports = app;
