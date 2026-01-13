680d28e6151451dc212679f974be788a03ac82cc
alexkmartinez77 <alexkmartinez77@gmail.com>
2026-01-13 15:53:58 -0800

    Makes sure headers are available on first pass

--- server/app.js
+++ server/app.js
@@ -7,6 +7,14 @@ const app = express();
 app.set('json spaces', 2);
 
 app.use((req, res, next) => {
+
+  const logPath = './server/log.csv';
+  const headers = 'userAgent;timestamp;method;url;protocol;statusCode';
+
+  if (!fs.existsSync(logPath) || fs.statSync(logPath).size === 0) {
+    fs.writeFileSync(logPath, headers);
+  }
+
   let log = (`${req.header('user-agent').replace(/;/g, '')};${new Date().toISOString()};${req.method};${req.originalUrl};HTTP/${req.httpVersion};${res.statusCode}`);
   fs.appendFile('./server/log.csv', `\n${log}`, (err) => err ? console.log(err) : console.log('Successful log!'));
   console.log(log);
